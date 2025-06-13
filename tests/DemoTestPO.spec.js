6dfda7c67a734c6cc42450b8a36ca4538e1a5aa7
DIPTISHETE <shetedipti4794@gmail.com>
2025-06-13 16:09:59 +0530

    Add test data import logic from JSON file for DemoTestPO

--- tests/DemoTestPO.spec.js
+++ tests/DemoTestPO.spec.js
@@ -1,26 +1,26 @@
 const { test, expect } = require('@playwright/test');
-const {POManager} = require ('../PageObjects/POManager')
+const {POManager} = require ('../PageObjects/POManager');
+//// Import test data from JSON file => json=>string => js object
+const dataset = JSON.parse(JSON.stringify(require('../utils/DemoTestPOTestData.json')));
+
 
 test('End-to-End Demo Test', async ({ page }) => {
 const poManager = new POManager(page);  // ✅ Correct instance
-  const username = "saloni10@gmail.com";
-  const password = "Saloni@9696";
-  const productName = "ZARA COAT 3";
-
+  
   // Step 1: Login
   const loginPage = poManager.getLoginPage(); // ✅ Use the instance
   await loginPage.goTo();
-  await loginPage.validLogin(username, password);
+  await loginPage.validLogin(dataset.username, dataset.password);
 
   // Step 2: Search and Add Product to Cart
   const dashboardPage = poManager.getDashboardPage(); // ✅ Use the instance
-  await dashboardPage.searchProductAddCart(productName);
+  await dashboardPage.searchProductAddCart(dataset.productName);
   await dashboardPage.navigateToCart();
 
 
 // Step 3: Verify that the selected product is present in the cart and proceed to checkout.
   const cartPage = poManager.getCartPage();
-  await cartPage.verifyProductIsDisplayed(productName);
+  await cartPage.verifyProductIsDisplayed(dataset.productName);
   await cartPage.checkOut();
 
   //step 4: Place an order by selecting country and capture the generated Order ID
