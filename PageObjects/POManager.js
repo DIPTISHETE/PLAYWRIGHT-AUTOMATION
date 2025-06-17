5c423712af34d251ae5165011963c4d73497b99b
DIPTISHETE <shetedipti4794@gmail.com>
2025-06-17 11:44:32 +0530

    some changes-completed cource

--- PageObjects/POManager.js
+++ PageObjects/POManager.js
@@ -1,49 +1,38 @@
-const {LoginPage} = require ('../PageObjects/LoginPage');
-const {DashboardPage} = require ('../PageObjects/DashboardPage');
-const {CartPage} = require ('../PageObjects/CartPage');
-const {OrderHistoryPage} = require ('../PageObjects/OrderHistoryPage');
+const { LoginPage } = require('../PageObjects/LoginPage');
+const { DashboardPage } = require('../PageObjects/DashboardPage');
+const { CartPage } = require('../PageObjects/CartPage');
 const { OrderReviewPage } = require('../PageObjects/OrderReviewPage');
+// const { OrderHistoryPage } = require('../PageObjects/OrderHistoryPage');
 
 class POManager {
   constructor(page) {
-
     this.page = page;
     this.loginPage = new LoginPage(this.page);
-    this.dashboardPage =  new DashboardPage(this.page);
+    this.dashboardPage = new DashboardPage(this.page);
     this.cartPage = new CartPage(this.page);
-//     this.orderhistoryPage  = new OrderHistoryPage (this.page);
-    this.orderreviewPage = new OrderReviewPage (this.page)
+    this.orderReviewPage = new OrderReviewPage(this.page);
+    // this.orderHistoryPage = new OrderHistoryPage(this.page);
   }
 
-getLoginPage ()
-{
-         return this.loginPage ;      
-}
+  getLoginPage() {
+    return this.loginPage;
+  }
 
-getDashboardPage ()
-{
-         return this.dashboardPage ;      
-}
+  getDashboardPage() {
+    return this.dashboardPage;
+  }
 
-getCartPage ()
-{
-                return this.cartPage ;
-}
+  getCartPage() {
+    return this.cartPage;
+  }
 
-getOrderReviewPage ()
-{
-       return this.orderreviewPage ;       
-}
+  getOrderReviewPage() {
+    return this.orderReviewPage;
+  }
 
-
-// getOrderHistoryPage ()
-// {
-//                 return this.orderhistoryPage ;
-// }
-
-
-
-
+  // getOrderHistoryPage() {
+  //   return this.orderHistoryPage;
+  // }
 }
 
 module.exports = { POManager };
